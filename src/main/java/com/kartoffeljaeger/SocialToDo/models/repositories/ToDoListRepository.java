c9cf7abd61f04c1f0c09b95ecc2d24a554280fdd
Arenile <55638040+Arenile@users.noreply.github.com>
2021-04-17 10:20:58 -0500

    Made ToDoListRepository.java

--- src/main/java/com/kartoffeljaeger/SocialToDo/models/repositories/ToDoListRepository.java
+++ src/main/java/com/kartoffeljaeger/SocialToDo/models/repositories/ToDoListRepository.java
@@ -1,5 +1,11 @@
 package com.kartoffeljaeger.SocialToDo.models.repositories;
 
-public class ToDoListRepository {
+import java.util.UUID;
+
+import com.kartoffeljaeger.SocialToDo.models.entities.ToDoListEntity;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface ToDoListRepository extends CrudRepository<ToDoListEntity, UUID>{
     
 }
