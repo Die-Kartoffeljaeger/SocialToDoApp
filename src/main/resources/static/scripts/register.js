a25f6dd361d29dc11361be1c62c08725a737cbec
Arenile <55638040+Arenile@users.noreply.github.com>
2021-04-17 11:40:50 -0500

    script for passwords matching

---
+++ src/main/resources/static/scripts/register.js
@@ -0,0 +1,8 @@
+function checkPassword(passedPassword, confirmPassword) {
+    if (passedPassword === confirmPassword) {
+            return true;
+    }
+    else {
+        return false;
+    }
+}
