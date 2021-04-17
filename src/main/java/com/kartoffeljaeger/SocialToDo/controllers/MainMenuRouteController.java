e5cf012c66a1fe536e27ad0a39d211e8c9b582ff
Arenile <55638040+Arenile@users.noreply.github.com>
2021-04-17 13:11:15 -0500

    MainMenuRouteController

--- src/main/java/com/kartoffeljaeger/SocialToDo/controllers/MainMenuRouteController.java
+++ src/main/java/com/kartoffeljaeger/SocialToDo/controllers/MainMenuRouteController.java
@@ -3,21 +3,33 @@ package com.kartoffeljaeger.SocialToDo.controllers;
 import javax.servlet.http.HttpServletRequest;
 
 import com.kartoffeljaeger.SocialToDo.controllers.enums.ViewNames;
+import com.kartoffeljaeger.SocialToDo.models.repositories.ActiveUserRepository;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
+import net.bytebuddy.dynamic.TypeResolutionStrategy.Active;
+
 @Controller
 @RequestMapping(value = "/")
 public class MainMenuRouteController
 {
+
 	@RequestMapping(method = RequestMethod.GET)
 	public ModelAndView start(final HttpServletRequest request)
 	{
 		// TODO: Use request to verify user is logged in (activeuser)
-
-		return new ModelAndView(ViewNames.MAIN_MENU.getViewName());
+		if (activeUserRepository.findBySessionKey(request.getSession().getId()).isPresent()) {
+			return new ModelAndView(ViewNames.MAIN_MENU.getViewName());
+		}
+		else {
+			return this.invalidSessionResponse();
+		}
 	}
+
+	@Autowired
+	ActiveUserRepository activeUserRepository;
 }
