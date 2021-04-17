48df2ed23ac9b4580a95fb2d78c709794bfa015a
Arenile <55638040+Arenile@users.noreply.github.com>
2021-04-17 12:06:23 -0500

    password matching function always else

--- src/main/resources/static/scripts/register.js
+++ src/main/resources/static/scripts/register.js
@@ -1,9 +1,12 @@
-function checkPassword(passedPassword, confirmPassword) {
-    if (passedPassword === confirmPassword) {
+function validateForm() {
+    if (document.forms["registerForm"]["password"] === document.forms["registerForm"]["confirmPassword"]) {
+        document.getElementById("mismatchError").style.display = "none";
         //Create a new user with that stuff
+        return true;
     }
     else {
-        document.getElementById("mismatchError").value =
+        document.getElementById("mismatchError").innerHTML =
             "Passwords do not match!";
+        return false;
     }
 }
