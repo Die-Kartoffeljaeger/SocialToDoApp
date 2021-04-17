95a830fc735c00b845b201e86fa3cf59e8a61d4a
Jackson Bullard <jsbullar@uark.edu>
2021-04-16 19:02:45 -0500

    Initial commit.

---
+++ src/main/java/com/kartoffeljaeger/SocialToDo/SocialToDoApplication.java
@@ -0,0 +1,13 @@
+package com.kartoffeljaeger.SocialToDo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SocialToDoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SocialToDoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kartoffeljaeger/SocialToDo/SocialToDoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kartoffeljaeger.SocialToDo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SocialToDoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
