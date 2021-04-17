8a6939660a68f8176dbcf0c5bf446460b0d42ea6
Arenile <55638040+Arenile@users.noreply.github.com>
2021-04-17 10:34:20 -0500

    Decided between conflicting changes in ToDoListRepository

--- src/main/java/com/kartoffeljaeger/SocialToDo/models/repositories/ToDoListRepository.java
+++ src/main/java/com/kartoffeljaeger/SocialToDo/models/repositories/ToDoListRepository.java
@@ -1,9 +1,6 @@
 package com.kartoffeljaeger.SocialToDo.models.repositories;
 
-<<<<<<< Updated upstream
-=======
 import java.util.Optional;
->>>>>>> Stashed changes
 import java.util.UUID;
 
 import com.kartoffeljaeger.SocialToDo.models.entities.ToDoListEntity;
@@ -11,10 +8,6 @@ import com.kartoffeljaeger.SocialToDo.models.entities.ToDoListEntity;
 import org.springframework.data.repository.CrudRepository;
 
 public interface ToDoListRepository extends CrudRepository<ToDoListEntity, UUID>{
-<<<<<<< Updated upstream
-    
-=======
     boolean existsById(UUID entryId);
     Optional<ToDoListEntity> findById(UUID entryId);
->>>>>>> Stashed changes
 }
